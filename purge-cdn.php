fa241e7a867ff8530052c1543751014141b97f96
sharul hafiz <sharulhafiz@gmail.com>
2014-04-15 14:33:05 +0800

    Clean up

--- purge-cdn.php
+++ purge-cdn.php
@@ -1,14 +1,13 @@
 <?php
-// Using composer
+// Using composer. Set your path here
 require_once '/Users/sharulhafiz/vendor/autoload.php';
 
+// CDN id
 $id = '64167';
 //$api->delete('/zones/pull.json/'.$id);
 
+// CDN Account Info
 $api = new NetDNA("pejabathalehwalk","2ba8ef1728afd0555d632d5f0e452d5505191d33d","0e5790b9292a74c84be15cb326a549a3");
-
-// get account information
-//echo  $api->get('/account.json');
 
 // delete a file from the cache
 $params = array('file' => '/dev/2014/js/central_api.js');
