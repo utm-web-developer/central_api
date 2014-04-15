38ec3455a22b7d990ecaa333b286e174f074b3b3
misterpah <misterpah@gmail.com>
2014-04-15 15:13:47 +0800

    repair facebook feed

--- interface/utm-facebook-feed.php
+++ interface/utm-facebook-feed.php
@@ -1,4 +1,21 @@
 <?php
-$access_token = "CAACEdEose0cBAAxaPghZAaajM535fwIqV38zejkQDjgfZAt1sHjnou2PE4FwjcioIsrs4xVy1er45HO3LawSovlxwkam3JI57kYCifCAP8MI6E3esU1Tet20vNybjJy8Eh3bMXzUNEpf9iQJrvWZApjgUbKrwDTRRln4buBRZACEMGJBqeZCQIPZBnzkuLDRLwE4vmy7NOlAZDZD";
-$content = file_get_contents("https://graph.facebook.com/univteknologimalaysia?fields=feed&access_token=".$access_token);
-$jsonResult = json_decode($content,true);
+require 'facebook-php-sdk/src/facebook.php';
+
+$facebook = new Facebook(array(
+  'appId'  => '290239217808174',
+  'secret' => 'b667d6feffff3c86c60306b96a7e7f9a',
+));
+
+/* make the API call */
+$response = $facebook->api(
+    "/univteknologimalaysia/feed",
+    "GET"
+);
+
+$message = array();
+for ($i=0; $i<count($response['data']); $i++)
+    {
+    array_push($message,array($response['data'][$i]['message'],$response['data'][$i]['created_time']));
+    }
+$jsonResult = $message;
+
